a4bd0c0bd55bfff1a2446059d52eae5b48cd4258
Brage <37003366+hellobafu@users.noreply.github.com>
2021-04-27 10:27:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,55 @@
 const Discord = require("discord.js");
+const chalk   = require("chalk");
+const fs      = require("fs");
+
+// --------------------------------------------------------------
+
+const { token, logo, prefix, logprefix } = require("./files/settings.json");
+const client = new Discord.Client({
+	ws: { intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS', 'GUILDS', 'GUILD_MESSAGES'] }
+});
+
+client.commands = new Discord.Collection();
+client.events   = new Discord.Collection();
+
+// --------------------------------------------------------------
+
+console.clear();
+console.log(logo);
+
+// --------------------------------------------------------------
+
+const getJsFiles = async (dir) => {
+	await fs.readdir(dir, (error, files) => {
+		if (error) console.error(error);
+
+		const commandFiles = files.filter(file => file.endsWith(".js"));
+		if (!commandFiles) return console.log(`${logprefix} No command files found.`);
+		
+		return files;
+	});
+	return;	
+}
+
+// -- Commands --------------------------------------------------
+
+getJsFiles("./commands/").then(commandFiles => {
+	commandFiles.forEach(file => {
+		const command = require(`./commands/${file}`);
+		client.commands.set(command.name, command);
+	});
+}
+
+// -- Events ----------------------------------------------------
+
+getJsFiles("./events/").then(eventFiles => {
+	eventFiles.forEach(file => {
+		const event = require(`./commands/${file}`);
+		client.events.set(event.name, event);
+	});
+}
+
+// -- Login ------------------------------------------------------
+
+client.login(token);
+	
