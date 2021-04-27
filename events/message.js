225240265675ca9b689fdca7527f0d091e70c452
Brage <37003366+hellobafu@users.noreply.github.com>
2021-04-27 11:20:01 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,24 @@
 module.exports = { name: "message" }
 
+// --------------------------------------------------------------
 
+module.exports.run = async (client, message) => {
+	const { channel, author, content } = message;
+	const prefix = "?";
 
-module.exports.run = async (client) => {
-	//
+// --------------------------------------------------------------
+
+	if (message.type === "PINS_ADD") return message.delete();
+	if (channel.tupe === "DM")       return;
+	if (author.bot)                  return;
+
+// --------------------------------------------------------------
+
+	const args = content.slice(prefix.length).trim().split(/\s+/g);
+	const commandname = args.shift().toLowerCase();
+
+// --------------------------------------------------------------
+
+	const command = client.commands.get(commandname);
+	if (command) command.run(message, args);
 }
