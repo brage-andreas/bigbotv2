d51d525208c106ecc090b2225283ca886ccb7684
Brage <37003366+hellobafu@users.noreply.github.com>
2021-04-27 14:08:26 +0200

    Create guildMemberRemove.js

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,29 @@
+
+const { channels, embedColourRed, embedURL } = null;
+
+module.exports = { name: "guildMemberRemove" }
+
+// --------------------------------------------------------------
+
+module.exports.run = async (member) => {
+	const { guild, user } = member;
+
+// --------------------------------------------------------------
+
+	const channel = guild.channels.cache.find(ch => c === ch.name) || guild.channels.cache.filter(ch => ch.type === "text").first();
+
+// --------------------------------------------------------------
+
+	const joinEmbed = new Discord.MessageEmbed()
+	.setColor(embedColourRed)
+	.setTitle("ÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆ HJØLP HAN DRO")
+	.setAuthor(user.tag, user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 }))
+	.setURL(embedURL)
+	.setDescription(`hadebra ${member}`)
+	.addField("Info", `• Laget: made\n\n• Kom: came`)
+	.setTimestamp();
+    
+	channel.send(joinEmbed).catch(console.error);
+    
+        }
+}
