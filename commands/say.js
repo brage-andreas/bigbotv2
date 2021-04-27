246f129d4a9e9d63fa79a157de856820c8da7c34
Brage <37003366+hellobafu@users.noreply.github.com>
2021-04-27 10:59:19 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: ["say"],
+	use: "say tekst",
+	about: "Si noe",
+	category: "",
+	cooldown: 0
+}
+
+
+module.exports.run = (message, args) => {
+	const channel = 
+	if (!args.length) return message.channel
