2facc46b6b9deab9ceb158fd54cbdc705b524b49
Brage <brage.spammail@gmail.com>
2021-05-04 11:15:15 +0200

    .

--- commands/activities.js
+++ commands/activities.js
@@ -1,19 +1,30 @@
 const chalk = require("chalk");
-const { botLog } = require("../files/auto.js");
+const { emoji, botLog } = require("../files/auto.js");
 
 // --------------------------------------------------------------
 
 module.exports = {
 	name: ["activities", "acts"],
-	use: "",
-	about: "",
-	category: "management|fun|info|support",
+	use: "activities @user|userID",
+	about: "Sender hva brukeren gjør",
+	category: "info",
 	cooldown: 0
 }
 
 // --------------------------------------------------------------
 
 module.exports.run = async (message, args) => {
-	
-    botLog(chalk `{grey Used} X`);
+	const { mentions, guild, client, channel } = message;
+
+    const member = mentions.members.first() || guild.members.cache.get(args[0]);
+    if (!member) {
+        message.react(emoji(client, "err"));
+        channel.send("Finner ikke brukeren.");
+        return;
+    }
+
+    const { presence, user }     = member;
+    const { activities, status } = presence;
+
+    botLog(chalk `{grey Used} ACTIVITIES {grey on} ${user.tag} {grey (${user.id})}`);
 }
