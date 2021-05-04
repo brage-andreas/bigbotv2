0b2b9f69eefa65ae2e9285b8abe99a6cc1a70f6c
Brage <brage.spammail@gmail.com>
2021-05-04 11:22:25 +0200

    .

--- commands/activities.js
+++ commands/activities.js
@@ -14,6 +14,7 @@ module.exports = {
 // --------------------------------------------------------------
 
 module.exports.run = async (message, args) => {
+    String.prototype.capitalise = function() { this.replace(/^\w/g, char => char.toUpperCase()) }
 	const { mentions, guild, client, channel } = message;
 
     const member = mentions.members.first() || guild.members.cache.get(args[0]);
@@ -25,6 +26,8 @@ module.exports.run = async (message, args) => {
 
     const { presence, user }     = member;
     const { activities, status } = presence;
+
+    const strStatus = status === "dnd" ? "Do Not Disturb" : status.capitalise();
 
     botLog(chalk `{grey Used} ACTIVITIES {grey on} ${user.tag} {grey (${user.id})}`);
 }
